2906f858bff5e0d9ccbe457eedf63044506eee9d
Svetlana <kotekina.sv@gmail.com>
2021-08-05 18:07:02 +0300

    com

---
+++ task3.py
@@ -0,0 +1,36 @@
+"""
+3. Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname, position (должность),
+ income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь,
+ содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+ Создать класс Position (должность) на базе класса Worker.
+  В классе Position реализовать методы получения полного имени сотрудника (get_full_name)
+  и дохода с учетом премии (get_total_income).
+ Проверить работу примера на реальных данных (создать экземпляры класса Position,
+  передать данные, проверить значения атрибутов, вызвать методы экземпляров).
+"""
+class Worker:
+    name = None
+    surname = None
+    position = None
+    __income = {"wage": 'wage', "bonus": 'bonus'}
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.__income = {"wage": 'wage', "bonus": 'bonus'}
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self.__income.get('wage') + self.__income.get('bonus')
+
+engineer = Position('Vladimir', 'Ivanov', 'engineer', 150, 50)
+print(engineer.get_full_name())
+#print(engineer.get_total_income())
